c1867a2d02757b7a0badfde6f204a72db1151e3c
Anders Hofstee <a.hofstee@catalpainternational.org>
2011-11-26 23:32:21 +0900

    setting up static file support

--- settings.py
+++ settings.py
@@ -2,6 +2,8 @@
 # vim: ai ts=4 sts=4 et sw=4
 # encoding=utf-8
 
+import os
+
 # -------------------------------------------------------------------- #
 #                          MAIN CONFIGURATION                          #
 # -------------------------------------------------------------------- #
@@ -86,7 +88,7 @@ INSTALLED_APPS = [
     "eav", # used by xforms
     "uni_form", # used by xforms
     "django_sorting", # used by groups
-    "pagination",
+    "pagination", # used by groups
     "rapidsms_xforms",
     "registration",
     "contact",
@@ -131,6 +133,47 @@ MIDDLEWARE_CLASSES = (
 
 #    'django.middleware.csrf.CsrfViewMiddleware',
 #    'django.middleware.csrf.CsrfResponseMiddleware',
+)
+
+# Static File support
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = ''
+
+
+STATICFILES_ROOT = os.getcwd() + '/static/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT =  os.getcwd()+'/static/'
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    #'django.template.loaders.eggs.Loader',
 )
 
 # -------------------------------------------------------------------- #
--- urls.py
+++ urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = staticfiles_urlpatterns()
+
+urlpatterns += patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
